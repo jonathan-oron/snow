e34d0a89eb70a67c911a7ce5badf498e92b45954
Jonathan Oron <jonathanoron@gmail.com>
2024-09-06 22:29:01 +0300

    Initial Snow Fall

---
+++ src/scenes/Snow.ts
@@ -0,0 +1,37 @@
+import { Scene } from "phaser";
+
+export class Snow extends Scene {
+  camera: Phaser.Cameras.Scene2D.Camera;
+  flakes: Phaser.GameObjects.Group;
+
+  constructor() {
+    super("Snow");
+  }
+
+  create() {
+    this.camera = this.cameras.main;
+    this.camera.setBackgroundColor(0x000000);
+
+    this.flakes = this.physics.add.group();
+
+    const graphics = this.make.graphics({ x: 0, y: 0 });
+    graphics.fillStyle(0xffffff);
+    // Loop through the bitmap array and draw it
+    graphics.fillPoint(0, 0, 2);
+
+    graphics.generateTexture("flake", 2, 2);
+    graphics.destroy(); // Clean up graphics object
+
+    for (let i = 0; i < 100; i++) {
+      const sprite = this.flakes.create(
+        Phaser.Math.FloatBetween(0, this.scale.width),
+        Phaser.Math.FloatBetween(0, this.scale.height - 100),
+        "flake"
+      );
+
+      sprite.setVelocity(0, Phaser.Math.FloatBetween(15, 25));
+    }
+  }
+
+  update(time: number, delta: number): void {}
+}
